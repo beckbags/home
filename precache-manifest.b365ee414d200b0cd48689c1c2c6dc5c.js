39ffd1b0fb4699ca03e6035f25f3e4abd9d6da14
contactbeckfriends <contactbeckfriends@gmail.com>
2020-11-22 15:31:20 +0530

    Updates

---
+++ precache-manifest.b365ee414d200b0cd48689c1c2c6dc5c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc84de49404f22939156a887c52df33",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8a600907caf4a0881c67",
+    "url": "/home/static/css/2.5b3af423.chunk.css"
+  },
+  {
+    "revision": "7b629f6a6256cc85b262",
+    "url": "/home/static/css/main.14fbd84b.chunk.css"
+  },
+  {
+    "revision": "8a600907caf4a0881c67",
+    "url": "/home/static/js/2.9e260361.chunk.js"
+  },
+  {
+    "revision": "3e32fc12b8b19d7754177bd1a210cf3c",
+    "url": "/home/static/js/2.9e260361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b629f6a6256cc85b262",
+    "url": "/home/static/js/main.531acba4.chunk.js"
+  },
+  {
+    "revision": "58c72bcdcc3ce8830b08",
+    "url": "/home/static/js/runtime-main.d99ad067.js"
+  },
+  {
+    "revision": "4ca86dcf33180cec39455e3b5ddba3fb",
+    "url": "/home/static/media/GT-Pressura-Pro-Regular.4ca86dcf.woff2"
+  },
+  {
+    "revision": "23475fcadf7adf710dd4b72d11b0c41d",
+    "url": "/home/static/media/GT-Walsheim-Pro-Condensed-Black.23475fca.woff2"
+  },
+  {
+    "revision": "d3e9b49b94bc3e9715a2331decebf96a",
+    "url": "/home/static/media/GT-Walsheim-Pro-Condensed-Regular.d3e9b49b.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/home/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/home/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/home/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/home/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/home/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  }
+]);

6e58d71f3e0d32d216b73f05e5a65e897cad9c89
contactbeckfriends <contactbeckfriends@gmail.com>
2020-11-23 17:47:27 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2e00e84a00c374ea8c6073c9d97bf6ea.js"
+  "/precache-manifest.2b5ee6c9d5953b6a44d3e5ea89ed1ea1.js"
 );
 
 self.addEventListener('message', (event) => {
